10a9764b4874075c08dc9763590bc67ee6f38f9e
Peter <peter@cacdi.com>
2022-12-06 15:53:28 +0800

    網頁版壓力測試

--- src/LoadTest.js
+++ src/LoadTest.js
@@ -1,72 +1,131 @@
 import './LoadTest.css';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import webSocket from 'socket.io-client';
 import { useNavigate } from 'react-router-dom';
 
-const URL = process.env.URL || 'http://localhost:3000';
-const MAX_CLIENTS = 1000;
-const POLLING_PERCENTAGE = 0.05;
-const CLIENT_CREATION_INTERVAL_IN_MS = 10;
-const EMIT_INTERVAL_IN_MS = 1000;
-
-let clientCount = 0;
-let lastReport = new Date().getTime();
-let packetsSinceLastReport = 0;
-
-const startTest = () => {
-  const createClient = () => {
-    // for demonstration purposes, some clients stay stuck in HTTP long-polling
-    const transports =
-      Math.random() < POLLING_PERCENTAGE ? ["polling"] : ["polling", "websocket"];
-
-    const socket = io(URL, {
-      transports,
-    });
-
-    setInterval(() => {
-      socket.emit("client to server event");
-    }, EMIT_INTERVAL_IN_MS);
-
-    socket.on("server to client event", () => {
-      packetsSinceLastReport++;
-    });
-
-    socket.on("disconnect", (reason) => {
-      console.log(`disconnect due to ${reason}`);
-    });
-
-    if (++clientCount < MAX_CLIENTS) {
-      setTimeout(createClient, CLIENT_CREATION_INTERVAL_IN_MS);
-    }
-  };
-
-  createClient();
-
-  const printReport = () => {
-    const now = new Date().getTime();
-    const durationSinceLastReport = (now - lastReport) / 1000;
-    const packetsPerSeconds = (
-      packetsSinceLastReport / durationSinceLastReport
-    ).toFixed(2);
-
-    console.log(
-      `client count: ${clientCount} ; average packets received per second: ${packetsPerSeconds}`
-    );
-
-    packetsSinceLastReport = 0;
-    lastReport = now;
-  };
-  setInterval(printReport, 5000);
-}
+const URL = 'https://utexpo.cacdidemo.com/loadTest';
 
 function LoadTest() {
+  const navigate = useNavigate()
   const [isTesting, setIsTesting] = useState(false)
+  const [parameters, setParameters] = useState({
+    maxClients: 1000,
+    longPollingPercentage: 0.05,
+    clientCreationInterval: 10,
+    emitInterval: 1000,
+  })
+  const {
+    maxClients,
+    longPollingPercentage,
+    clientCreationInterval,
+    emitInterval
+  } = parameters
+  const onChangeParameter = (evt) => {
+    const newParameters = {
+      ...parameters,
+      [evt.target.name]: evt.target.value
+    }
+    setParameters(newParameters)
+  }
+
+
+  let clientCount = 0;
+  let lastReport = new Date().getTime();
+  let packetsSinceLastReport = 0;
+  let printJob = useRef()
+  let emitJobs = useRef([])
+  const results = useRef([])
+
+  const startTest = () => {
+    setIsTesting(true)
+    printJob.current = undefined
+    emitJobs.current = []
+    results.current = []
+    const createClient = () => {
+      // for demonstration purposes, some clients stay stuck in HTTP long-polling
+      const transports =
+        Math.random() < longPollingPercentage ? ['polling'] : ['polling', 'websocket'];
+  
+      const socket = webSocket(URL, {
+        transports,
+      });
+  
+      const emitJob = setInterval(() => {
+        socket.emit('client to server event');
+      }, emitInterval);
+      emitJobs.current.push(emitJob)
+  
+      socket.on('server to client event', () => {
+        packetsSinceLastReport++;
+      });
+  
+      socket.on('disconnect', (reason) => {
+        console.log(`disconnect due to ${reason}`);
+      });
+  
+      if (++clientCount < maxClients) {
+        setTimeout(createClient, clientCreationInterval);
+      }
+    };
+  
+    createClient();
+  
+    const printReport = () => {
+      const now = new Date().getTime();
+      const durationSinceLastReport = (now - lastReport) / 1000;
+      const packetsPerSeconds = (
+        packetsSinceLastReport / durationSinceLastReport
+      ).toFixed(2);
+      results.current.push({
+        id: new Date().getTime,
+        clientCount,
+        packetsPerSeconds,
+      })
+      packetsSinceLastReport = 0;
+      lastReport = now;
+    };
+    printJob.current = setInterval(printReport, 2000);
+  }
+
+  const stopTest = () => {
+    for (const job of emitJobs.current) {
+      clearInterval(job);
+    }
+    clearInterval(printJob.current)
+    setIsTesting(false)
+  }
+
   return (
     <div>
-      <div className="Actions">
-        {!isTesting && <button>開始測試</button>}
-        {!!isTesting && <button>停止測試</button>}
-      </div>
+        {!isTesting && (
+          <div className="Parameters">
+            <label>
+              總訪客數
+              <input value={maxClients} onChange={onChangeParameter} name="maxClients"/>
+            </label>
+            <label>
+              Long Polling 比例(0 ~ 1)
+              <input value={longPollingPercentage} onChange={onChangeParameter} name="longPollingPercentage"/>
+            </label>
+            <label>
+              訪客生成間隔(毫秒)
+              <input value={clientCreationInterval} onChange={onChangeParameter} name="clientCreationInterval"/>
+            </label>
+            <label>
+              訪客發送封包間隔(毫秒)
+              <input value={emitInterval} onChange={onChangeParameter} name="emitInterval"/>
+            </label>
+          </div>
+        )}
+        {!isTesting && <button onClick={startTest} className="Start-btn">開始測試</button>}
+        {!!isTesting && <div className="Waiting">測試中</div>}
+        {!!isTesting && <button onClick={stopTest} className="Stop-btn">Stop Testing</button>}
+        {!isTesting && !!results.current.length && <div className="Results">
+          {results.current.map(result => (
+            <div key={result.id}>訪客人數: {result.clientCount} 封包數(每秒): {result.packetsPerSeconds}</div>
+          ))}
+        </div>}
+        <button className="Navigate-btn" onClick={() => { navigate('/app') }}>回到首頁</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LoadTest from './LoadTest';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -14,7 +15,7 @@ const router = createBrowserRouter([
   },
   {
     path: '/test',
-    element: <div>Hello world!</div>,
+    element: <LoadTest />,
   },
 ]);
 
